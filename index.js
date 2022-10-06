33716ff640321f7d237069b42d9100241c064627
lolade-olatunji <ololade0507@gmail.com>
2022-10-06 11:09:38 +0100

    explore

--- index.js
+++ index.js
@@ -22,15 +22,20 @@ function closemenu(){
     sidemenu.style.right = "-200px";
 }
 
-<script>
-  const scriptURL = 'https://script.google.com/macros/s/
-  AKfycbzR2vi9TY-zTYGo7Oef9KruugD86gxk7KdXvZ7rCMm7-WoKT_vZWos4N7hkzqbDuMSY/exec'
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbzR2vi9TY-zTYGo7Oef9KruugD86gxk7KdXvZ7rCMm7-WoKT_vZWos4N7hkzqbDuMSY/exec'
   const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
 
   form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
+      .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+      })
       .catch(error => console.error('Error!', error.message))
-  })
-</script>
+  });
